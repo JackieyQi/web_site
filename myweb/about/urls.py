29a1e7cc5e581d34d18416bff511eae869327585
shdiaod <jackieyqi@gmail.com>
2020-12-13 10:59:24 +0800

    Del python2 code

--- myweb/about/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import about_view
-
-
-urlpatterns = [
-	url(r'^$', about_view, name="index"),
-]
--- myweb/labels/urls.py
+++
@@ -1,9 +0,0 @@
-# coding:utf8
-
-from django.conf.urls import url
-from labels.views import LabelIndexView
-
-urlpatterns = [
-	url(r'^(?P<label>[-\w]+)/$', LabelIndexView.as_view(), name="label"),
-
-]
